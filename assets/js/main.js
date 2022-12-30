429ad290667b6fbc3ef055991fa87834ba9980fc
MarkDev <markwebdev@mail.ru>
2022-12-30 16:07:09 +0300

    Остался футер и анимации

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.querySelector('.menu-btn'),
     menuButtonIcon = document.querySelector('.menu-btn-icon'),
-    menu = document.querySelector('.menu__responsive');
+    menu = document.querySelector('.menu__responsive'),
+    questionsList = document.querySelector('.questions__list');
+
+    const questionHandler = (e) => {
+        if(e.target.className === 'questions__list-item-button' || e.target.className === 'questions__list-item-button-icon'){
+            const parent = e.target.closest('.questions__list-item');
+            parent.classList.toggle('active');
+        }
+    };
+
 
     const menuHandler = () => {
         if(menu.classList.contains('active')){
@@ -13,4 +22,5 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     menuButton.addEventListener('click', () => menuHandler());
+    questionsList.addEventListener('click', (e) => questionHandler(e));
 });
